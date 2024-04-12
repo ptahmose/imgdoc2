0921c3968259f5a10516344bad09a10c4ffdf9ff
ptahmose <jbohl@h-quer.de>
2024-04-13 01:15:00 +0200

    cosmetic

--- dotnet/imgdoc2Net/Interfaces/ImgDoc2VersionInfo.cs
+++ dotnet/imgdoc2Net/Interfaces/ImgDoc2VersionInfo.cs
@@ -4,75 +4,15 @@
 
 namespace ImgDoc2Net.Interfaces
 {
-    /// <brief> 
-    /// Version information about the native imgdoc2 library.
-    /// </brief>
-    public struct ImgDoc2NativeLibraryVersionInfo
-    {
-        /// <summary> Gets or sets the major version.</summary>
-        /// <value> The major version.</value>
-        public int Major { get; set; }
-
-        /// <summary> Gets or sets the minor version.</summary>
-        /// <value> The minor version.</value>
-        public int Minor { get; set; }
-
-        /// <summary> Gets or sets the patch version.</summary>
-        /// <value> The patch version.</value>
-        public int Patch { get; set; }
-
-        /// <summary> Gets or sets the compiler identification - an informative string identifying the compiler.</summary>
-        /// <value> The compiler identification.</value>
-        public string CompilerIdentification { get; set; }
-
-        /// <summary> Gets or sets the type of the build - an information string identifying the build type, e.g. "Debug" or "Release".</summary>
-        /// <value> The build type.</value>
-        public string BuildType { get; set; }
-
-        /// <summary> Gets or sets URL of the repository.</summary>
-        /// <value> The repository URL.</value>
-        public string RepositoryUrl { get; set; }
-
-        /// <summary> Gets or sets the repository branch.</summary>
-        /// <value> The repository branch.</value>
-        public string RepositoryBranch { get; set; }
-
-        /// <summary> Gets or sets the repository tag.</summary>
-        /// <value> The repository tag.</value>
-        public string RepositoryTag { get; set; }
-    }
-
-    /// <brief> 
-    /// Version information about the managed imgdoc2 interop library.
-    /// </brief>
-    public struct ImgDoc2ManagedLibraryVersionInfo
-    {
-        /// <summary> Gets or sets the major version.</summary>
-        /// <value> The major version.</value>
-        public int Major { get; set; }
-
-        /// <summary> Gets or sets the minor version.</summary>
-        /// <value> The minor version.</value>
-        public int Minor { get; set; }
-
-        /// <summary> Gets or sets the patch version.</summary>
-        /// <value> The patch version.</value>
-        public int Patch { get; set; }
-
-        /// <summary> Gets or sets the reversion - this is always zero.</summary>
-        /// <value> The reversion - always 0.</value>
-        public int Revision { get; set; }
-
-        /// <summary> Gets or sets the type of the build - an information string identifying the build type, e.g. "Debug" or "Release".</summary>
-        /// <value> The build type.</value>
-        public string BuildType { get; set; }
-    }
-
     /// <summary> Version information about the ImgDoc2Net library.</summary>
     public class ImgDoc2VersionInfo
     {
+        /// <summary> Gets or sets the native library version.</summary>
+        /// <value> The native library version.</value>
         public ImgDoc2NativeLibraryVersionInfo NativeLibraryVersion { get; set; }
 
+        /// <summary> Gets or sets information describing the managed image document 2 library version.</summary>
+        /// <value> Information describing the managed image document 2 library version.</value>
         public ImgDoc2ManagedLibraryVersionInfo ManagedImgDoc2LibraryVersionInfo { get; set; }
     }
 }
